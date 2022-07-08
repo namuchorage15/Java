0b2962845b964ea18b8b74ea118fe58f92de4ad5
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-08 20:22:52 +0530

    Add files via upload

---
+++ BeansExample1.java
@@ -0,0 +1,34 @@
+package Mine;
+
+import java.util.List;
+
+public class BeansExample1 {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	}
+	
+
+
+
+
+	
+
+
+
+
+	
+
+
---
+++ Test.java
@@ -0,0 +1,17 @@
+package Mine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		BeansExample1 B1=new BeansExample1();
+		B1.setId(20);
+		System.out.println("Id value :-"+B1.getId());
+		B1.setName("soma");
+		System.out.println("name value:-"+B1.getName());
+		String Array[]= {"computer","IT","CIVIL","MECH"};
+		
+		
+	}
+}
