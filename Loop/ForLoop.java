e290f4f801d0dbd45f8c078d91ea2f197f604317
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-14 11:36:25 +0530

    Add files via upload

---
+++ Loop/ForLoop.java
@@ -0,0 +1,15 @@
+package Mine;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		for(int i=10;i>6;i--)
+		{
+		System.out.println(i);
+
+	}
+
+}
+}
---
+++ Loop/doWhileLoop.java
@@ -0,0 +1,14 @@
+package Mine;
+
+public class doWhileLoop {
+	public static void main(String args[]) {
+		int i=11;
+		do
+		{
+			System.out.println(i);
+			i--;
+		}while(i>3);
+	}
+
+}
+
