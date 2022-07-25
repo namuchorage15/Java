4600b94100206dfbc1f318bb3d55ce4e682fd91a
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-25 13:36:20 +0530

    Add files via upload

---
+++ Encapusulation/emp.java
@@ -0,0 +1,33 @@
+package Mine;
+
+public class emp {
+	private String EmpName;
+	private int EmpAge;
+	private int EmpAaddhar;
+	public String getEmpName()
+	{
+		return EmpName;
+	}
+	public void setEmpName(String empName)
+	{
+		EmpName = empName;
+	}
+	public int getEmpAge()
+	{
+		return EmpAge;
+	}
+	public void setEmpAge(int empAge) 
+	{
+		EmpAge = empAge;
+	}
+	public int getEmpAaddhar()
+	{
+		return EmpAaddhar;
+	}
+	public void setEmpAaddhar(int empAaddhar)
+	{
+		EmpAaddhar = empAaddhar;
+	}
+	
+
+}
---
+++ Encapusulation/empdetails.java
@@ -0,0 +1,18 @@
+package Mine;
+
+public class empdetails {
+	public static void main(String[] args) 
+	{
+		emp E1=new emp();
+		E1.setEmpName("Ram");
+		E1.setEmpAge(25);
+		E1.setEmpAaddhar(487651234);
+		System.out.println("Emp Name:"+E1.getEmpName());
+		System.out.println("Emp Name:"+E1.getEmpAge());
+		System.out.println("Emp Name:"+E1.getEmpAaddhar());
+		
+
+	}
+
+}
+
